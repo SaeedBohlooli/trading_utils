e9587e875d073d8bb4a10c3ddadef97fe72d803a
Saeed Bohlooli <s.bohlooli@gmail.com>
2025-11-04 18:01:20 -0500

    Check in code ...

--- ib_utils.py
+++ ib_utils.py
@@ -1,8 +1,18 @@
+import os.path
+
 import pandas as pd
 import logging
 
 
-
+df_file_map = {
+    "ib_on_fill_fill_df": f"85-ib_on_fill_fill_df.csv",
+    "ib_on_fill_trade_df": f"86-ib_on_fill_trade_df.csv",
+    "ib_portfolio_df": f"87-ib_portfolio_df.csv",
+    "ib_commission_df": f"88-ib_commission_df.csv",
+    "ib_commission_fill_df": f"89-ib_commission_fill_df.csv",
+    "ib_commission_trade_df": f"90-ib_commission_trade_df.csv",
+    "ib_execution_df": f"91-ib_execution_df.csv",
+}
 
 import datetime
 from ib_insync import *
@@ -69,22 +79,35 @@ def on_fill(trade, fill):
 
     return
 
+
+
 def save_ib_dfs(portfolio_dir, ib):
-    flatten_on_fill_fill_df_file_path = f"{portfolio_dir}/85-ib_on_fill_fill_df.csv"
-    df_utils.save_df_to_csv_a_tabular(global_state.ib_on_fill_fill_df, file_path=flatten_on_fill_fill_df_file_path, mode='a')
-    flatten_on_fill_trade_df_file_path = f"{portfolio_dir}/86-ib_on_fill_trade_df.csv"
-    df_utils.save_df_to_csv_a_tabular(global_state.ib_on_fill_trade_df, file_path=flatten_on_fill_trade_df_file_path, mode='a')
-    ib_portfolio_df_file_path = f"{portfolio_dir}/87-ib_portfolio_df.csv"
-    df_utils.save_df_to_csv_a_tabular(global_state.ib_portfolio_df, file_path=ib_portfolio_df_file_path, mode='a')
-    ib_commission_df_file_path = f"{portfolio_dir}/88-ib_commission_df.csv"
-    df_utils.save_df_to_csv_a_tabular(global_state.ib_commission_df, file_path=ib_commission_df_file_path, mode='a')
-    ib_commission_fill_df_file_path = f"{portfolio_dir}/89-ib_commission_fill_df.csv"
-    df_utils.save_df_to_csv_a_tabular(global_state.ib_commission_fill_df, file_path=ib_commission_fill_df_file_path, mode='a')
-    ib_commission_trade_df_file_path = f"{portfolio_dir}/90-ib_commission_trade_df.csv"
-    df_utils.save_df_to_csv_a_tabular(global_state.ib_commission_trade_df, file_path=ib_commission_trade_df_file_path, mode='a')
+
     generate_ib_execution_df(ib) # save in the global_state
-    ib_execution_df_file_path = f"{portfolio_dir}/91-ib_execution_df.csv"
-    df_utils.save_df_to_csv_a_tabular(global_state.ib_execution_df, file_path=ib_execution_df_file_path,mode='a')
+
+    for df_name, file_name in df_file_map.items():
+
+        df = getattr(global_state, df_name, None)
+        if df is not None:
+            file_path = f"{portfolio_dir}/{file_name}"
+            df_utils.save_df_to_csv_a_tabular(df, file_path=file_path, mode='a')
+
+
+    if False:
+        flatten_on_fill_fill_df_file_path = f"{portfolio_dir}/85-ib_on_fill_fill_df.csv"
+        df_utils.save_df_to_csv_a_tabular(global_state.ib_on_fill_fill_df, file_path=flatten_on_fill_fill_df_file_path, mode='a')
+        flatten_on_fill_trade_df_file_path = f"{portfolio_dir}/86-ib_on_fill_trade_df.csv"
+        df_utils.save_df_to_csv_a_tabular(global_state.ib_on_fill_trade_df, file_path=flatten_on_fill_trade_df_file_path, mode='a')
+        ib_portfolio_df_file_path = f"{portfolio_dir}/87-ib_portfolio_df.csv"
+        df_utils.save_df_to_csv_a_tabular(global_state.ib_portfolio_df, file_path=ib_portfolio_df_file_path, mode='a')
+        ib_commission_df_file_path = f"{portfolio_dir}/88-ib_commission_df.csv"
+        df_utils.save_df_to_csv_a_tabular(global_state.ib_commission_df, file_path=ib_commission_df_file_path, mode='a')
+        ib_commission_fill_df_file_path = f"{portfolio_dir}/89-ib_commission_fill_df.csv"
+        df_utils.save_df_to_csv_a_tabular(global_state.ib_commission_fill_df, file_path=ib_commission_fill_df_file_path, mode='a')
+        ib_commission_trade_df_file_path = f"{portfolio_dir}/90-ib_commission_trade_df.csv"
+        df_utils.save_df_to_csv_a_tabular(global_state.ib_commission_trade_df, file_path=ib_commission_trade_df_file_path, mode='a')
+        ib_execution_df_file_path = f"{portfolio_dir}/91-ib_execution_df.csv"
+        df_utils.save_df_to_csv_a_tabular(global_state.ib_execution_df, file_path=ib_execution_df_file_path,mode='a')
 
     return
 def generate_ib_execution_df(ib):
@@ -118,3 +141,12 @@ def test_me():
     global_state.application_state = "SET IN THE UTILS ..."
 
 
+def load_ib_df(portfolio_dir, df_name='ib_on_fill_fill_df'):
+    df_file_name = df_file_map.get(df_name, None)
+    if df_file_name is None:
+        return  pd.DataFrame()
+    file_path = f"{portfolio_dir}/{df_file_name}"
+    if os.path.exists(file_path):
+        df = pd.read_csv(file_path)
+        return df
+    return pd.DataFrame()
