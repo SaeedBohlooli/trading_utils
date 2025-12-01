dab482add72b2babcaebb6f4c9260b451562825a
Saeed Bohlooli <s.bohlooli@gmail.com>
2025-12-01 11:46:13 -0500

    Check in code from p349

--- config_utils.py
+++ config_utils.py
@@ -7,11 +7,10 @@ logger = logging.getLogger(__name__)
 import yaml
 
 def load_config(file_path ='config.yaml') -> dict:
+    config = {}
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
             config = yaml.safe_load(file)
-    else:
-        config = {}
 
     file_path = file_path.replace('.yaml', '.local.yaml')
 
@@ -28,19 +27,21 @@ def load_config(file_path ='config.yaml') -> dict:
 
 
 def load_app_config(portfolio_id, config_folder='',load_coommon=True):
+
+    config = {}
     if config_folder == '':
         configs_folder = f'../configs'
-    logger.info(f"loading app_config ....")
-    app_config = load_config(f'{configs_folder}/config-{portfolio_id}.yaml')
-    logger.info(f"loaded.")
+
     if load_coommon:
         logger.info(f"loading  common config ....")
-        common_config = load_config(f'{configs_folder}/config-common.yaml')
-        if common_config != None and common_config != {}:
-            app_config.update(common_config)
-            logger.info(f"Common config loaded.")
-        else:
-            logger.warning(f"No common config found. or empty.")
+        config = load_config(f'{configs_folder}/config-common.yaml')
+        logger.info(f"Common config loaded.")
+
+
+    logger.info(f"loading app_config ....")
+    app_config = load_config(f'{configs_folder}/config-{portfolio_id}.yaml')
+    app_config.update(config)
+    logger.info(f"loaded.")
 
     return app_config
 
