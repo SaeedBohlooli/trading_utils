9e568ae8d7c688c5f631f422245fc9bfdc3976d7
Saeed Bohlooli <s.bohlooli@gmail.com>
2025-12-11 16:29:06 -0500

    from u102

--- send_requests.py
+++ send_requests.py
@@ -5,23 +5,32 @@ print('# ###########################')
 host= '127.0.0.1'
 port = 5102
 if True:
-    d = {
-        "request_type": "buy_order",
-        "base_strike": "6850",
-        "expiry": 20251211,
-        "bid_price" :-10,
-        "web_request_id": "test_request_003",
-        "memo": "Test buy order from send_requests.py",
-        'status': 'PY_SENT'
-    }
-
     # d = {
-    #     "order_set_id": "SET-20251210-140109--206",  # TODO u need to change it every time
+    #     "request_type": "buy_order",
+    #     "base_strike": "6870",
+    #     "expiry": 2025112,
+    #     "bid_price" :-8,
+    #     "web_request_id": "test_request_003",
+    #     "memo": "Test buy order from send_requests.py",
+    #     'status': 'PY_SENT'
+    # }
+
+    d = {
+      "request_type": "BUY_ORDER",
+      "base_strike": 6900,
+      "user_price": -.95,
+      "expiry": "20251212",
+      "web_request_id": "saeed-1",
+      "web_timestamp": "2025-12-11 16:44:18",
+      "status": "WEB_SENT"
+    }
+    # d = {
+    #     "order_set_id": "SET-20251211-105818--113",  # TODO u need to change it every time
     #     "base_strike": 6850,
     #     "expiry": 20251211,
-    #     "bid_price" : -21.50,
+    #     "user_price" : -21.50,
     #     "web_request_id": "100",
-    #     "request_type": "close_order",
+    #     "request_type": "CLOSE_ORDER_SET",
     #     "memo": "A request from Web to close order ",
     # 'status': 'PY_SENT'
     # }
