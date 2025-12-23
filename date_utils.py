cdf48cc83ab0c5fe9f5bd4167c51f3884c930498
Saeed Bohlooli <s.bohlooli@gmail.com>
2025-12-22 20:39:25 -0500

    from u102

--- date_utils.py
+++ date_utils.py
@@ -21,6 +21,11 @@ def time_now_yyyy_mm_dd_hh_mm():
 def time_now_yyyy_mm_dd_hh_mm_ss():
     now_date_time = datetime.datetime.now()
     return now_date_time.strftime("%Y-%m-%d %H:%M:%S")
+
+def time_now_yyyy_mm_dd_hh_mm_ss_as_id():
+    now_date_time = datetime.datetime.now()
+    return now_date_time.strftime("%Y%m%d-%H%M%S")
+
 def format_yyyymmdd(date_obj):
     """
     Convert a datetime.date (or datetime.datetime) to yyyymmdd string.
