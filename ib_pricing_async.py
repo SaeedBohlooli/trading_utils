b142f5a7718976b701dc9f9696383e29e183e50f
bs24680@gmail.com <Yek00Yek@>
2025-12-29 21:27:23 -0500

    from p106- server

--- ib_pricing_async.py
+++ ib_pricing_async.py
@@ -231,8 +231,8 @@ async def get_or_subscribe_symbol_price(ib,
     while True:
         q = global_state.quote_cache.get(con_id)
 
-        logger.debug(f"@@ get_latest_price: symbol: {symbol}, con_id: {con_id}, global_state.quote_cache: {global_state.quote_cache}")
-        logger.debug(f"@@ get_latest_price: symbol: q: {q}")
+        logger.debug(f"@ get_latest_price: symbol: {symbol}, con_id: {con_id}, global_state.quote_cache: {global_state.quote_cache}")
+        logger.debug(f"@ get_latest_price: symbol: q: {q}")
 
         if q:
             last = q.get("last")
@@ -247,19 +247,19 @@ async def get_or_subscribe_symbol_price(ib,
         # No price yet
         # -----------------------------
         if not wait_for_price:
-            logger.warning(f"@@ get_latest_price: No quote yet for {symbol}")
+            logger.warning(f"@ get_latest_price: No quote yet for {symbol}")
             return None
 
         if time.time() - start > timeout_sec:
             logger.warning(
-                f"@@ Timeout waiting for price for {symbol} after {timeout_sec}s"
+                f"@ Timeout waiting for price for {symbol} after {timeout_sec}s"
             )
             return None
 
         if not warned:
-            logger.info(f"@@ Waiting for first price for {symbol}")
+            logger.info(f"@ Waiting for first price for {symbol}")
             warned = True
-        logger.info(f"@@ get_latest_price: No valid price yet for {symbol} ... we still waiting ...")
+        logger.info(f"@ get_latest_price: No valid price yet for {symbol} ... we still waiting ...")
         await asyncio.sleep(poll_interval)
 
 def valid(x):
